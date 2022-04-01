98a293029f69b4ce3cae34590376efe845597f62
ctrl-raul <mail.raulvitor@gmail.com>
2022-04-01 06:50:12 -0300

    Updated the wu command to just send the link

--- src/commands/wu.cmd.ts
+++ src/commands/wu.cmd.ts
@@ -1,5 +1,4 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
-import { MessageEmbed } from 'discord.js'
 import { Command } from '../CommandsManager'
 
 
@@ -11,23 +10,11 @@ const SUPERMECHS_WORKSHOP_URL = 'https://workshop-unlimited.vercel.app/'
 export const command: Command = {
 
   data: new SlashCommandBuilder()
-		.setName('wu')
-		.setDescription('Link SuperMechs Workshop'),
+    .setName('wu')
+    .setDescription('Link SuperMechs Workshop'),
 
-	execute (interaction) {
-
-    interaction.reply({
-      embeds: [
-        new MessageEmbed({
-          color: '#112233',
-          fields: [{
-            name: 'SuperMechs Workshop:',
-            value: SUPERMECHS_WORKSHOP_URL
-          }]
-        })
-      ]
-    })
-
-	},
+  execute (interaction) {
+    interaction.reply({ content: SUPERMECHS_WORKSHOP_URL })
+  },
 
 }
