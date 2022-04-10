c6b4dd15e7adb93e0aaedbd7a1d7da35ba6e3ea9
ctrl-raul <mail.raulvitor@gmail.com>
2022-04-10 18:11:21 -0300

    Changed the command a lil bit

--- src/commands/badmechfuni.cmd.ts
+++ src/commands/badmechfuni.cmd.ts
@@ -8,12 +8,12 @@ export const command: Command = {
 
   data: new SlashCommandBuilder()
     .setName('badmechfuni')
-    .setDescription('Tells whoever is posting bad mechs thinking they are funny to shut up'),
+    .setDescription('Compliments the funny mech poster'),
 
   execute (interaction) {
 
     const user = interaction.options.getUser('user')
-    const reply = 'wow 🤯 the ❗ MECH 🤖 you 🧍 just 🕔 sent 📧 is 🤔 HILARIOUS 😂 i 🤖 am ❕ CRYING 😭 of ❓ LAUGHTER 🤣 right ➡️ now 🕒 it\'s 😱 so 😩 much 💯 that 👉 i 🤖 think 💭 i 🤖 am ❕ going 📝 to ❗ EXPLODE 💥 of 📴 joy 😀 you 🧍 are 🤔 so 😩 original 💡 nobody 🔎 ever 🌎 did 🏃 this ✨ before ⌛ thank 🙌 you ❤️'
+    const reply = 'wow 🤯 the ❗ MECH 🤖 you 🧍 just 🕔 posted 📧 is 🤔 HILARIOUS 😂 i 🤖 am ❕ CRYING 😭 of ❓ LAUGHTER 🤣 right ➡️ now 🕒 it\'s 😱 so 😩 much 💯 that 👉 i 🤖 think 💭 i 🤖 am ❕ going 📝 to ❗ EXPLODE 💥 of 📴 joy 😀 you 🧍 are 🤔 so 😩 ORIGINAL 💡 nobody 🔎 EVER 🌎 did 🏃 this ✨ before ⌛ thank 🙌 you ❤️'
 
     interaction.reply({
       content: user ? `<@${user.id}> ${reply}` : reply
